a52398cdb1f726ac62db0ca2e79b8e7449b376d8
vishalkrsoni <vishal.sony1@gmail.com>
2023-02-13 10:43:05 +0530

    final UI deploy

--- client/src/PortfolioContainer/Testimonial/Testimonial.js
+++ client/src/PortfolioContainer/Testimonial/Testimonial.js
@@ -1,4 +1,4 @@
-import React,{useState,useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import OwlCarousel from "react-owl-carousel";
 import "owl.carousel/dist/assets/owl.carousel.css";
 import "owl.carousel/dist/assets/owl.theme.default.css";
@@ -47,23 +47,22 @@ export default function Testimonial(props) {
     },
   };
 
+  const [showScroll, handleShowScroll] = useState(false);
+  const transitionScrollButton = () => {
+    if (window.scrollY > 100) {
+      handleShowScroll(true);
+    } else {
+      handleShowScroll(false);
+    }
+  };
 
-   const [showScroll, handleShowScroll] = useState(false);
-   const transitionScrollButton = () => {
-     if (window.scrollY > 100) {
-       handleShowScroll(true);
-     } else {
-       handleShowScroll(false);
-     }
-   };
-
-   useEffect(() => {
-     window.addEventListener("scroll", transitionScrollButton);
-     return () => window.removeEventListener("scroll", transitionScrollButton);
-   }, []);
+  useEffect(() => {
+    window.addEventListener("scroll", transitionScrollButton);
+    return () => window.removeEventListener("scroll", transitionScrollButton);
+  }, []);
 
   return (
-    <div id={props.id || ""}  className="testimonial__container">
+    <div id={props.id || ""} className="testimonial__container">
       <ScreenHeading
         title={"Testimonials"}
         subHeading={"What My Peers Say About Me"}
@@ -81,12 +80,11 @@ export default function Testimonial(props) {
                   <div className="testi-comment">
                     <p>
                       <i className="fa fa-quote-left" />
-                      We build a
-                      self-sustainable ecosystem for our FMCG Start-Up together. He is a
-                      gem of a person. He can figure out the problem areas and
-                      execute real-time changes quickly. He has also gained
-                      extensive experience with operations management and
-                      workflow managament.
+                      We build a self-sustainable ecosystem for our FMCG
+                      Start-Up together. He is a gem of a person. He can figure
+                      out the problem areas and execute real-time changes
+                      quickly. He has also gained extensive experience with
+                      operations management and workflow managament.
                       <i className="fa fa-quote-right" />
                     </p>
                     <ul className="stars list-unstyled">
@@ -145,9 +143,9 @@ export default function Testimonial(props) {
                   <div className="testi-comment">
                     <p>
                       <i className="fa fa-quote-left" />
-                      Working with Vishal was good and we had
-                      really high expectations. He is always ready to learn new
-                      things by doing all the research about best practices and
+                      Working with Vishal was good and we had really high
+                      expectations. He is always ready to learn new things by
+                      doing all the research about best practices and
                       implementing them. I believe he is a very talented and
                       favourible employee, one who is always ready to take
                       challenges.
@@ -206,10 +204,10 @@ export default function Testimonial(props) {
                     <p>
                       <i className="fa fa-quote-left" />
                       Vishal deeply understands his work and gets things done
-                      efficiently in accordance with best practices. The entire management expresses gratitude to
-                      him. Along with adding value in the product developement
-                      and adding valuable ideas time to time, he always get
-                      things done on time.
+                      efficiently in accordance with best practices. The entire
+                      management expresses gratitude to him. Along with adding
+                      value in the product developement and adding valuable
+                      ideas time to time, he always get things done on time.
                       <i className="fa fa-quote-right" />
                     </p>
                     <ul className="stars list-unstyled">
@@ -265,11 +263,10 @@ export default function Testimonial(props) {
                     <p>
                       <i className="fa fa-quote-left" />
                       I have worked with Vishal during PrepLeaf phase.He always
-                      supported  and exceeded our expectations in every way.
-                      Not only was our experience friendly, his
-                      ability to identify and present our key messages in an
-                      imaginative and creative way gave us a huge amount of
-                      confidence in him
+                      supported and exceeded our expectations in every way. Not
+                      only was our experience friendly, his ability to identify
+                      and present our key messages in an imaginative and
+                      creative way gave us a huge amount of confidence in him
                       <i className="fa fa-quote-right" />
                     </p>
                     <ul className="stars list-unstyled">
@@ -342,7 +339,7 @@ export default function Testimonial(props) {
           className={`scroll__up__btn ${
             showScroll ? "show__scroll" : "hide__scroll"
           }`}
-          onClick={() => ScrollService.scrollHandler.scrollToHome()}>
+          onClick={() => ScrollService.scrollToHome()}>
           <FontAwesomeIcon className="arrow__up__icon" icon={faUpLong} />
         </button>
       </div>
