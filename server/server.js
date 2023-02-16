1162afefbefeca2ebe7f3279f5157bfc731ab9af
vishalkrsoni <vishal.sony1@gmail.com>
2023-02-16 19:26:44 +0530

    mobile responsiveness

--- server/server.js
+++ server/server.js
@@ -2,14 +2,21 @@ const nodemailer = require("nodemailer");
 const express = require("express");
 const port = 8080;
 const app = express();
+const cors = require("cors");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const { SMTP_HOST, SMTP_USER, SMTP_PASSWORD, GMAIL_EMAIL, GMAIL_PASSWORD } =
+  process.env;
 
 const mailerConfig = () => {
   return nodemailer.createTransport({
-    host: "smtp.ethereal.email",
-    port: 587,
+    host: SMTP_HOST,
+    port: 465,
+    secure: true,
     auth: {
-      user: "benton.wolff22@ethereal.email",
-      pass: "F8gxVwKE2GvfEZNjRn",
+      user: SMTP_USER,
+      pass: SMTP_PASSWORD,
     },
   });
 };
@@ -20,11 +27,12 @@ const sendEmail = async (transporter, name, email, message) => {
       return console.error("Name, email and message are required fields");
     }
     let mailOptions = {
-      from: `"${name}" <${email}>`,
-      to: "vishal.sony1@gmail.com",
-      subject: "Message from portfolio",
+      from: GMAIL_EMAIL,
+      to: "vishalkrsoni1@gmail.com",
+      subject: `message from ${name}--Portfolio`,
       text: message,
     };
+
     await transporter.sendMail(mailOptions, (error, info) => {
       if (error) {
         throw new Error(`Error sending email: ${error}`);
@@ -39,32 +47,35 @@ const sendEmail = async (transporter, name, email, message) => {
 
 const sendContactInfo = async (req, res) => {
   const { name, email, message } = req.body;
-
-  if (!name || !email || !message) {
+  if (name.length === 0 || email.length === 0 || message.length === 0) {
     return res.status(400).send({
       error: "Name, email, and message are required fields.",
       status: "incomplete info",
+      message: "incomplete info",
     });
   }
 
   const transporter = mailerConfig();
-
   try {
     await sendEmail(transporter, name, email, message);
     res.status(200).send({
       success: true,
       message: "Email sent successfully",
+      status: "success",
     });
   } catch (error) {
     console.error(`Error sending email: ${error}`);
     res.status(500).send({
       error,
-      message:
-        "An error occurred while sending the email. Please try again later.",
+      message: "An error occurred, Please try again later.",
     });
   }
+
 };
 
+
+
+app.use(cors());
 app.use(express.json());
 app.post("/contact", sendContactInfo);
 
