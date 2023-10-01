9b80a346d398fc4d31b28d7fe9cd8ac05eb694a8
vishalkrsoni <vishal.sony1@gmail.com>
2023-10-01 17:39:24 +0530

    using nodemailer to send message

--- server/router/contactMeRouter.js
+++ server/router/contactMeRouter.js
@@ -1,10 +1,19 @@
 const { Router } = require('express');
 const contactRouter = Router();
 const {
-  StatusCodes: { METHOD_NOT_ALLOWED },
+  StatusCodes: { METHOD_NOT_ALLOWED, OK },
 } = require('http-status-codes');
 
 const { sendContactInfo } = require('../controller/contactMeController');
+
+contactRouter.get('/', (req, res) => {
+  res.status(OK).send({
+    message: 'Health check success',
+    statusCode: OK,
+    status: 'success',
+    data: null,
+  });
+});
 
 contactRouter.post('/contact', sendContactInfo).all('/contact', (req, res) => {
   res.status(METHOD_NOT_ALLOWED).json({
